263b4351db618d392bc30dd480108ba135ed72f2
IngDavidTM <david-1011@hotmail.com>
2023-01-19 12:42:44 -0500

    Add user spec methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,8 @@ RSpec.describe User, type: :model do
     subject.posts_counter = -1
     expect(subject).to_not be_valid
   end
+
+  it 'recent posts should return the last three posts' do
+    expect(subject.recent_posts).to eq(subject.posts.last(3))
+  end
 end
