3f6f85ec4c19dee0a70c1c595b03f829ecdd3ef0
IngDavidTM <david-1011@hotmail.com>
2023-01-19 12:26:14 -0500

    Add user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'post should be an integer' do
+    subject.posts_counter = 'not-numeric'
+    expect(subject).to_not be_valid
+  end
+
+  it 'post should be greater than or equal to 0' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+end
