e13d35d427cafad87c236344abe2e32bb061a20b
IngDavidTM <david-1011@hotmail.com>
2023-01-25 21:24:31 -0500

    Add html structure to show post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:id])
-    @posts = Post.where(author_id: params[:id])
+    @user = User.find_by(id: params[:user_id])
+    @posts = Post.where(author_id: params[:user_id])
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find_by(id: params[:id])
     @comments = Comment.where(posts_id: params[:id])
   end
 end
