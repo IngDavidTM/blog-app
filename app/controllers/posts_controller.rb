283bf0967be85c6ac29f72c8c8d48131c7a2635d
IngDavidTM <david-1011@hotmail.com>
2023-01-25 20:50:18 -0500

    Add variables to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:id])
+    @posts = Post.where(author_id: params[:id])
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @comments = Comment.where(posts_id: params[:id])
+  end
 end
