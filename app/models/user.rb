1d44d217f4da8f082ef7a75cd1e472586da92873
IngDavidTM <david-1011@hotmail.com>
2023-01-31 19:38:47 -0500

    Add devise functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
-
+  has_one_attached :photo
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
   has_many :posts, foreign_key: :author_id
