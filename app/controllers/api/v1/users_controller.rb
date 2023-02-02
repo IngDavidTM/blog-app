0997e43344987a5ca5417f4c014b07ddb690661b
IngDavidTM <david-1011@hotmail.com>
2023-02-02 15:28:20 -0500

    Add API for posts for each user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -38,7 +38,7 @@ class Api::V1::UsersController < Api::V1::ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = user.find(params[:id])
+      @user = User.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
