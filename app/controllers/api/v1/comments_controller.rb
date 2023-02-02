8e012e06184e3ffcced72d4dd42024dac04b8052
IngDavidTM <david-1011@hotmail.com>
2023-02-02 16:31:23 -0500

    Add API for post comments

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,39 @@
+class Api::V1::CommentsController < Api::V1::ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
+
+  def create
+    @comment = Comment.new(params[:id])
+    @comment.author = current_user
+
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+   # PATCH/PUT /comments/1
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /comments/1
+  def destroy
+    @comment.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.require(:comment).permit(:id, :text)
+    end
+end
